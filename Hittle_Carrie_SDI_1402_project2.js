8a518aca01a19fe666f6576ee7e357d6d305e2ca
Carrie Cass-Hittle <curdlelee70@gmail.com>
2014-02-13 22:18:20 -0500

    project 2
    2nd commit

--- Hittle_Carrie_SDI_1402_project2.js
+++ Hittle_Carrie_SDI_1402_project2.js
@@ -4,50 +4,27 @@
 // "Keeping Track"
 // a "TripnDizzy Ink Production"
 alert("WELCOME....to....Keeping Track");
-
-
-var member = true;
+var myConfirmation = false;
+var membership = true + email;
 var email = "";
-var confirmation = confirm("Are you a member of Keeping Track?");{
-	if(confirmation === true){
-		prompt("Sign in please." + email);
-		}else{
-			console.log("Join us."); 
-          alert("Feel free to check us out, or better yet start Keeping Track!");
-			}
-		}
-var input = prompt("Please choose a catogorie",[0-3]);
+var membershipInfo = membership;
 var catArray = [ "movies","music","photos"];
-if(input === catArray[1]){
+
+myConfirmation = confirm("Are you a member of Keeping Track?");
+if(myConfirmation === false) {
+	console.log("Feel free to check us out, or better yet start Keeping Track!");
+	alert("Join us.");
+	} else {
+			myPrompt = prompt("Sign in please." + email);
+			};
+prompt("Please choose a catogorie");
+
+
+(input === catArray[1]){
 	console.log("I see you have choosen to check out our movies program, great choice.");
-	if(input === catArray[2]){
+	(input === catArray[2]){
 		console.log("I see you have choosen to check out our music program, great choice.");
 		}else{
 			console.log("I see you have choosen to check out our photo program, great choice.");
 			}
 		}
-
-
-/*var number
-var boolean
-
-
-output vars
-
-prompt for input
-
-boolean condition
-
-true prompt input
-number conditional
-math
-outputA
-true outputB
-
-false prompt for input
-string conditional
-true outputC
-false string concatenation
-outputD
-
-end*/
